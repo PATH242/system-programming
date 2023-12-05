efc3c11177b217aea7423fed47cba5fadea5c702
PATH242 <mennashaheen2357@gmail.com>
2023-12-05 03:46:49 +0300

    15 points solution, missing a small piece
    It doesn't always pass the stress test, but passes the rest witho
    ut memory leaks. Will double check the blocking or non-blocking situation in server because it's unable to accept new connection at a certain point...

--- lab5/chat_client.c
+++ lab5/chat_client.c
@@ -1,31 +1,60 @@
 #include "chat.h"
 #include "chat_client.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <poll.h>
+#define _GNU_SOURCE
+#define BUF_SIZE 1024
 struct chat_client {
 	/** Socket connected to the server. */
 	int socket;
 	/** Array of received messages. */
-	/* ... */
+	int n_received_messages;
+	struct chat_message** received_messages;
+	/** Array of to be sent messages. */
+	int n_messages_to_be_sent;
+	struct chat_message** messages_to_be_sent;
 	/** Output buffer. */
-	/* ... */
+	char* output_buf;
+	int output_buf_size;
+	int output_buf_capacity;
 	/* PUT HERE OTHER MEMBERS */
-	const char* name;
+	char* name;
+	struct pollfd* poll_trigger;
 };
 
 struct chat_client *
 chat_client_new(const char *name)
 {
-	/* Ignore 'name' param if don't want to support it for +5 points. */
-	(void)name;
 
-	struct chat_client *client = calloc(1, sizeof(*client));
+	struct chat_client* client = malloc(sizeof(struct chat_client));
+	if (client == NULL)
+	{
+        perror("Error allocating memory for chat client\n");
+        abort();
+    }
 	client->socket = -1;
-
-	/* IMPLEMENT THIS FUNCTION */
-
+	client->n_received_messages = 0;
+	client->received_messages = NULL;
+	client->n_messages_to_be_sent = 0;
+	client->messages_to_be_sent = NULL;
+	client->output_buf = calloc(2, sizeof(char));
+	client->output_buf_size = 0;
+	client->output_buf_capacity = 2;
+	client->name = strdup(name);
+	client->poll_trigger = calloc(1, sizeof(struct pollfd));
 	return client;
 }
 
@@ -34,49 +63,237 @@ chat_client_delete(struct chat_client *client)
 {
 	if (client->socket >= 0)
 		close(client->socket);
-
-	/* IMPLEMENT THIS FUNCTION */
-
+	for(int i = 0; i < client->n_received_messages; i++)
+	{
+		chat_message_delete(client->received_messages[i]);
+	}
+	if(client->received_messages != NULL)
+	{
+		free(client->received_messages);
+	}
+	for(int i = 0; i < client->n_messages_to_be_sent; i++)
+	{
+		chat_message_delete(client->messages_to_be_sent[i]);
+	}
+	if(client->messages_to_be_sent != NULL)
+	{
+		free(client->messages_to_be_sent);
+	}
+	if(client->output_buf != NULL)
+	{
+		free(client->output_buf);
+	}
+	free(client->poll_trigger);
+	free(client->name);
 	free(client);
 }
 
 int
 chat_client_connect(struct chat_client *client, const char *addr)
 {
-	/*
-	 * 1) Use getaddrinfo() to resolve addr to struct sockaddr_in.
-	 * 2) Create a client socket (function socket()).
-	 * 3) Connect it by the found address (function connect()).
-	 */
-	/* IMPLEMENT THIS FUNCTION */
-	(void)client;
-	(void)addr;
 
-	return CHAT_ERR_NOT_IMPLEMENTED;
+	if (client->socket != -1)
+	{
+        return CHAT_ERR_ALREADY_STARTED;
+    }
+	struct addrinfo *addr_info;
+	struct addrinfo filter;
+    memset(&filter, 0, sizeof(filter));
+	filter.ai_family = AF_INET;
+	filter.ai_socktype = SOCK_STREAM;
+	char* addr_copy = strdup(addr);
+	char *host = strtok(addr_copy, ":");
+    char *port = strtok(NULL, ":");
+	int rc = getaddrinfo(host, port, &filter, &addr_info);
+	free(host);
+	// free(port);
+    if (rc != 0) {
+        perror("Error resolving address");
+		close(client->socket);
+		freeaddrinfo(addr_info);
+        return CHAT_ERR_NO_ADDR;
+    }
+	if(addr_info == NULL)
+	{
+		perror("Error finding address\n");
+		close(client->socket);
+		freeaddrinfo(addr_info);
+        return CHAT_ERR_NO_ADDR;
+	}
+	for(struct addrinfo* i = addr_info; i != NULL; i = i->ai_next)
+	{
+		client->socket = socket(i->ai_family, i->ai_socktype, i->ai_protocol);
+		rc = connect(client->socket, i->ai_addr, i->ai_addrlen);
+		if(rc < 0)
+		{
+			close(client->socket);
+			continue;
+		}
+		break;
+	}
+	if(client->socket == -1)
+	{
+		perror("Error creating socket");
+        return CHAT_ERR_SYS;
+	}
+	if(rc != 0)
+	{
+		perror("port busy? or like connection failed\n");
+		return CHAT_ERR_PORT_BUSY;
+	}
+	client->poll_trigger->fd = client->socket;
+	client->poll_trigger->events = POLLIN;
+	freeaddrinfo(addr_info);
+	return 0;
 }
 
 struct chat_message *
 chat_client_pop_next(struct chat_client *client)
 {
-	/* IMPLEMENT THIS FUNCTION */
-	(void)client;
+	if(client->n_received_messages)
+	{
+		struct chat_message* ret = client->received_messages[0];
+		for(int i = 1; i < client->n_received_messages; i++)
+		{
+			client->received_messages[i-1]
+				= client->received_messages[i];
+		}
+		client->n_received_messages -= 1;
+		client->received_messages = 
+				realloc(client->received_messages, client->n_received_messages * sizeof(struct chat_message*));
+		return ret;
+	}
 	return NULL;
+}
+
+int chat_client_send_buf(struct chat_client* client)
+{
+	int n_sent = 0;
+	int total_sent = 0;
+	if(client->n_messages_to_be_sent)
+	{
+		char* msg = client->messages_to_be_sent[0]->data;
+		int sz = strlen(msg) + 1;
+		msg[sz-1] = '\n';
+		while(sz)
+		{
+			n_sent = send(client->socket, msg+n_sent, sz, 0);
+			if(n_sent < 0)
+			{
+				return CHAT_ERR_SYS;
+			}
+			else
+			if(n_sent <= 0)
+			{
+				close(client->socket);
+				client->socket = -1;
+				return 0;
+			}
+			else
+			{
+				sz -= n_sent;
+				total_sent += n_sent;
+			}
+		}
+		chat_message_delete(client->messages_to_be_sent[0]);
+		for(int i = 1; i < client->n_messages_to_be_sent; i++)
+		{
+			client->messages_to_be_sent[i-1] = client->messages_to_be_sent[i];
+		}
+		client->n_messages_to_be_sent -= 1;
+		client->messages_to_be_sent = realloc(client->messages_to_be_sent,
+							client->n_messages_to_be_sent * sizeof(struct chat_message*));
+	}
+
+	if(!client->n_messages_to_be_sent)
+	{
+		client->poll_trigger->events = POLLIN;
+	}
+	return total_sent;
+}
+
+int chat_client_receive_buf(struct chat_client* client)
+{
+	int rc = 0, n_received = 0;
+	char* input_buf = calloc(BUF_SIZE, sizeof(char));
+	int input_buf_capacity = BUF_SIZE;
+	do{
+		n_received += rc;
+		if(n_received == input_buf_capacity)
+		{
+			input_buf_capacity *= 2;
+			char* new_buf = calloc(input_buf_capacity, sizeof(char));
+			memcpy(new_buf, input_buf, n_received);
+			free(input_buf);
+			input_buf = new_buf;
+		}
+		rc = recv(client->socket, input_buf + n_received, input_buf_capacity - n_received, MSG_DONTWAIT);
+	}while(rc > 0);
+	if(n_received == 0)
+	{
+		free(input_buf);
+		return 0;
+	}
+	else
+	{
+		int start = 0;
+		for(int i = 0; i < n_received; i++)
+		{
+			if(input_buf[i] == '\n')
+			{
+				// printf("received one message of size %d\n", i - start + 1);
+				struct chat_message* new_message = malloc(sizeof(struct chat_message));
+				char* message_content = calloc(i - start + 1, sizeof(char));
+				message_content = memcpy(message_content, input_buf+start, (i - start +1));
+				// add terminating character instead of '\n'
+				message_content[i-start] = '\0';
+				new_message->data = message_content;
+				client->received_messages = realloc(client->received_messages, (client->n_received_messages + 1) * sizeof(struct chat_message*));
+				client->received_messages[client->n_received_messages] = new_message;
+				client->n_received_messages += 1;
+				start = i + 1;
+			}
+		}
+	}
+	free(input_buf);
+	// printf("received smth in client here\n");
+	return n_received;
 }
 
 int
 chat_client_update(struct chat_client *client, double timeout)
 {
-	/*
-	 * The easiest way to wait for updates on a single socket with a timeout
-	 * is to use poll(). Epoll is good for many sockets, poll is good for a
-	 * few.
-	 *
-	 * You create one struct pollfd, fill it, call poll() on it, handle the
-	 * events (do read/write).
-	 */
-	(void)client;
-	(void)timeout;
-	return CHAT_ERR_NOT_IMPLEMENTED;
+	if(client->socket == -1)
+	{
+		return CHAT_ERR_NOT_STARTED;
+	}
+	if(timeout < 0)
+	{
+		timeout = 0;
+	}
+
+	int rc = poll(client->poll_trigger, 1, (int) timeout * 1000);
+	if(rc == 0)
+	{
+		return CHAT_ERR_TIMEOUT;
+	}
+	else 
+	if(rc < 0)
+	{
+		perror("Error getting events at client update");
+		return CHAT_ERR_SYS;
+	}
+	if(client->poll_trigger->revents & POLLOUT)
+	{
+		// printf("got to send buf\n");
+		chat_client_send_buf(client);
+	}
+	if(client->poll_trigger->revents & POLLIN)
+	{
+		// printf("got to receive buf\n");
+		chat_client_receive_buf(client);
+	}
+	return 0;
 }
 
 int
@@ -88,20 +305,106 @@ chat_client_get_descriptor(const struct chat_client *client)
 int
 chat_client_get_events(const struct chat_client *client)
 {
-	/*
-	 * IMPLEMENT THIS FUNCTION - add OUTPUT event if has non-empty output
-	 * buffer.
-	 */
-	(void)client;
+	if(client->socket == -1)
+	{
+		return 0;
+	}
+	if((client->poll_trigger->events) & POLLOUT)
+	{
+		return (CHAT_EVENT_INPUT | CHAT_EVENT_OUTPUT);
+	}
 	return CHAT_EVENT_INPUT;
+}
+
+// Trim message from extra spaces in the front and back
+uint32_t trim_message(char* msg, int msg_size)
+{
+	int n_zeroes_front = 0, n_zeroes_back = 0;
+	for(int i = 0; i < msg_size; i++)
+	{
+		if(msg[i] == ' ')
+		{
+			n_zeroes_front ++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	if(n_zeroes_front)
+	{
+		// todo: do it in a faster way mbe?
+		for(int i = 0; i < msg_size; i++)
+		{
+			msg[i - n_zeroes_front] = msg[i];
+		}
+	}
+	for(int i = msg_size - 2; i >= 0; i--)
+	{
+		if(msg[i] == ' ')
+		{
+			n_zeroes_back ++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	if(n_zeroes_back)
+	{
+		msg[msg_size- n_zeroes_back -1] = '\n';
+	}
+	return msg_size - n_zeroes_front - n_zeroes_back;
 }
 
 int
 chat_client_feed(struct chat_client *client, const char *msg, uint32_t msg_size)
 {
-	/* IMPLEMENT THIS FUNCTION */
-	(void)client;
-	(void)msg;
-	(void)msg_size;
-	return CHAT_ERR_NOT_IMPLEMENTED;
+	if(client->socket == -1)
+	{
+		return CHAT_ERR_NOT_STARTED;
+	}
+	int target_size = msg_size + client->output_buf_size;
+	if(target_size > client->output_buf_capacity)
+	{
+		client->output_buf_capacity = target_size;
+		client->output_buf = realloc(client->output_buf, target_size * sizeof(char));
+	}
+	// printf("feedinggg\n");
+	memcpy(client->output_buf+client->output_buf_size,
+	 	msg, msg_size);
+	int start = 0;
+	for(int i = client->output_buf_size; i < target_size; i++)
+	{
+		if(client->output_buf[i] == '\n')
+		{
+			int sz = i - start + 1;
+			// printf("made one message of size %d\n", sz);
+			struct chat_message* new_message = malloc(sizeof(struct chat_message));
+			char* message_content = calloc(sz, sizeof(char));
+			message_content = memcpy(message_content, client->output_buf+start, sz);
+			sz = trim_message(message_content, sz);
+			// add terminating char instead of '\n', will be reverted above.
+			message_content[sz - 1] = '\0';
+			new_message->data = message_content;
+			client->messages_to_be_sent = realloc(
+							client->messages_to_be_sent, (client->n_messages_to_be_sent + 1) * sizeof(struct chat_message*));
+			client->messages_to_be_sent[client->n_messages_to_be_sent] = new_message;
+			client->n_messages_to_be_sent += 1;
+			start = i + 1;
+		}
+	}
+	client->output_buf_size = (int)target_size;
+	if(start)
+	{
+		client->poll_trigger->events |= POLLOUT;
+		client->output_buf_size -= start;
+		char* new_buf = calloc(client->output_buf_capacity - start, sizeof(char));
+		client->output_buf_capacity -= start;
+		memcpy(new_buf, client->output_buf+start, client->output_buf_size);
+		free(client->output_buf);
+		client->output_buf = new_buf;
+	}
+	// printf("I fed it %uld\n", msg_size);
+	return 0;
 }
--- lab5/chat_server.c
+++ lab5/chat_server.c
@@ -2,34 +2,67 @@
 #include "chat_server.h"
 
 #include <netinet/in.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <fcntl.h>
+#define _GNU_SOURCE
+#define BUF_SIZE 1024
 
 struct chat_peer {
 	/** Client's socket. To read/write messages. */
 	int socket;
+	char* name;
 	/** Output buffer. */
-	/* ... */
-	/* PUT HERE OTHER MEMBERS */
+	char* output_buf;
+	int output_buf_size;
+	int output_buf_capacity;
 };
 
 struct chat_server {
 	/** Listening socket. To accept new clients. */
 	int socket;
 	/** Array of peers. */
-	/* ... */
-	/* PUT HERE OTHER MEMBERS */
+	struct chat_peer** peers;
+	int n_peers;
+	/** Array of received messages. */
+	int n_received_messages;
+	struct chat_message** received_messages;
+	/** Input buffer. */
+	char* input_buf;
+	int input_buf_size;
+	int input_buf_capacity;
+
+	struct epoll_event epoll_trigger;
+	int epoll_fd;
 };
 
 struct chat_server *
 chat_server_new(void)
 {
-	struct chat_server *server = calloc(1, sizeof(*server));
+	struct chat_server *server = malloc(sizeof(*server));
+	if(server == NULL)
+	{
+		abort();
+	}
 	server->socket = -1;
-
-	/* IMPLEMENT THIS FUNCTION */
-
+	server->n_peers = 0;
+	server->peers = NULL;
+	server->input_buf = NULL;
+	server->input_buf_size = 0;
+	server->input_buf_capacity = 0;
+	server->n_received_messages = 0;
+	server->received_messages = NULL;
 	return server;
 }
 
@@ -37,56 +70,377 @@ void
 chat_server_delete(struct chat_server *server)
 {
 	if (server->socket >= 0)
+	{
 		close(server->socket);
-
-	/* IMPLEMENT THIS FUNCTION */
-
+	}
+	for(int i = 0; i < server->n_peers; i++)
+	{
+		if(server->peers[i]->name != NULL)
+		{
+			free(server->peers[i]->name);
+		}
+		if(server->peers[i]->output_buf != NULL)
+		{
+			free(server->peers[i]->output_buf);
+		}
+		free(server->peers[i]);
+	}
+	if(server->n_peers)
+	{
+		free(server->peers);
+	}
+	if(server->input_buf_capacity)
+	{
+		free(server->input_buf);
+	}
+	for(int i = 0; i < server->n_received_messages; i++)
+	{
+		free(server->received_messages[i]);
+	}
+	if(server->received_messages != NULL)
+	{
+		free(server->received_messages);
+	}
 	free(server);
 }
 
+void delete_peer(struct chat_server* server, int fd)
+{
+	// printf("This client %d is getting deleted\n", fd);
+	short int shift = 0;
+	for(int i = 0; i < server->n_peers; i++)
+	{
+		if(server->peers[i]->socket == fd)
+		{
+			if(server->peers[i]->output_buf_capacity)
+			{
+				free(server->peers[i]->output_buf);
+			}
+			if(server->peers[i]->name != NULL)
+			{
+				free(server->peers[i]->name);
+			}
+			free(server->peers[i]);
+			shift = 1;
+		}
+		if(shift && i + 1 < server->n_peers)
+		{
+			server->peers[i] = server->peers[i+1];
+		}
+	}
+	if(shift)
+	{
+		server->n_peers --;
+		server->peers = realloc(server->peers, server->n_peers * sizeof(struct chat_peer*));
+	}
+	return;
+}
+
+int accept_new_peer(struct chat_server* server)
+{
+	int new_client_fd = accept(server->socket, NULL, NULL);
+	if(new_client_fd < 0)
+	{
+		return CHAT_ERR_SYS;
+	}
+	// printf("I'm accepting this client: %d\n", new_client_fd);
+	struct chat_peer* new_peer = malloc(sizeof(struct chat_peer));
+	// bonus todo: get name
+	new_peer->name = NULL;
+	new_peer->socket = new_client_fd;
+	new_peer->output_buf_size = 0;
+	new_peer->output_buf_capacity = 0;
+	new_peer->output_buf = NULL;
+
+	server->peers = realloc(server->peers, (server->n_peers + 1) * sizeof(struct chat_peer*));
+	server->peers[server->n_peers] = new_peer;
+	server->n_peers += 1;
+
+	struct epoll_event event;
+	event.events = (EPOLLIN | EPOLLET);
+	event.data.fd = new_client_fd;
+	// Make the new connection non blocking
+	int old_flags = fcntl(new_client_fd, F_GETFL);
+	fcntl(new_client_fd, F_SETFL, old_flags | O_NONBLOCK);
+ 	if(epoll_ctl(server->epoll_fd, EPOLL_CTL_ADD, new_client_fd, &event) < 0)
+	{
+		epoll_ctl(server->epoll_fd, EPOLL_CTL_DEL, new_client_fd, NULL);
+		close(new_client_fd);
+		free(new_peer);
+		return CHAT_ERR_SYS;
+	}
+	// printf("Now I have %d peers\n", server->n_peers);
+	return 0;
+}
+
+struct chat_peer* find_peer(struct chat_server* server, int client_fd)
+{
+	for(int i = 0; i < server->n_peers; i++)
+	{
+		if(server->peers[i]->socket == client_fd)
+		{
+			return server->peers[i];
+		}
+	}
+	return NULL;
+}
+
+/*
+ * 1) Create a server socket (function socket()).
+ * 2) Bind the server socket to addr (function bind()).
+ * 3) Listen the server socket (function listen()).
+ * 4) Create epoll
+ */
 int
 chat_server_listen(struct chat_server *server, uint16_t port)
 {
+	if (server->socket != -1)
+	{
+        return CHAT_ERR_ALREADY_STARTED;
+    }
+	
 	struct sockaddr_in addr;
 	memset(&addr, 0, sizeof(addr));
 	addr.sin_port = htons(port);
 	/* Listen on all IPs of this machine. */
 	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server->socket  = socket(AF_INET, SOCK_STREAM, 0);
 
-	/*
-	 * 1) Create a server socket (function socket()).
-	 * 2) Bind the server socket to addr (function bind()).
-	 * 3) Listen the server socket (function listen()).
-	 * 4) Create epoll/kqueue if needed.
-	 */
-	/* IMPLEMENT THIS FUNCTION */
-	(void)server;
+	if (server->socket < 0) {
+        perror("Error creating server socket");
+        return CHAT_ERR_SYS;
+    }
 
-	return CHAT_ERR_NOT_IMPLEMENTED;
+    if (bind(server->socket, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("Error binding server socket");
+        return CHAT_ERR_SYS;
+    }
+	if(listen(server->socket, 16) < 0)
+	{
+		perror("Error listening on server");
+		close(server->socket);
+		return CHAT_ERR_SYS;
+	}
+	
+	int old_flags = fcntl(server->socket, F_GETFL);
+	fcntl(server->socket, F_SETFL, old_flags | O_NONBLOCK);
+	int ep = epoll_create(1);
+	server->epoll_fd = ep;
+	if (ep < 0)
+	{
+		perror("Error creating epoll\n");
+		close(server->socket);
+		return CHAT_ERR_SYS;
+	}
+	server->epoll_trigger.events = (EPOLLIN | EPOLLET);
+	server->epoll_trigger.data.fd = server->socket;
+	if(epoll_ctl(ep, EPOLL_CTL_ADD, server->socket, &server->epoll_trigger) < 0)
+    {
+		perror("Error adding server socket to epoll");
+        close(server->socket);
+		close(ep);
+        return CHAT_ERR_SYS;
+    }
+    return 0;
 }
 
 struct chat_message *
 chat_server_pop_next(struct chat_server *server)
 {
-	/* IMPLEMENT THIS FUNCTION */
-	(void)server;
+	if(server->n_received_messages)
+	{
+		struct chat_message* ret = server->received_messages[0];
+		for(int i = 1; i < server->n_received_messages; i++)
+		{
+			server->received_messages[i-1]
+				= server->received_messages[i];
+		}
+		server->n_received_messages--;
+		server->received_messages = 
+			realloc(server->received_messages, (server->n_received_messages) * sizeof(struct chat_message*));
+		return ret;
+	}
 	return NULL;
 }
 
+int send_message_to_client(struct chat_server* server, int client_socket)
+{
+	struct chat_peer* client = find_peer(server, client_socket);
+	if(client == NULL)
+	{
+		return -1;
+	}
+	if(client->output_buf_size == 0)
+	{
+		return 0;
+	}
+	int pointer = 0;
+	while(pointer < client->output_buf_size)
+	{
+		pointer += 
+			send(client_socket, client->output_buf + pointer,
+					client->output_buf_size - pointer, 0);
+	}
+	server->epoll_trigger.events = (EPOLLET | EPOLLIN);
+	server->epoll_trigger.data.fd = client_socket;
+	int rc = epoll_ctl(server->epoll_fd, EPOLL_CTL_MOD, client_socket, &server->epoll_trigger);
+	client->output_buf_size = 0;
+	client->output_buf_capacity = 0;
+	free(client->output_buf);
+	client->output_buf = NULL;
+	if(rc < 0)
+	{
+		return CHAT_ERR_SYS;
+	}
+	return 0;
+}
+
+int send_message_to_clients(struct chat_server* server, struct epoll_event* event)
+{
+	// printf("tryna send messages to all clients and all for this client: %d\n", event->data.fd);
+	for(int i = 0; i < server->n_peers; i++)
+	{
+		int client_socket = server->peers[i]->socket;
+		if(client_socket == event->data.fd)
+		{
+			continue;
+		}
+		struct chat_peer* client = find_peer(server, client_socket);
+		int target_size = client->output_buf_size + server->input_buf_size;
+		char* new_buf = calloc(target_size, sizeof(char));
+		memcpy(new_buf, client->output_buf, client->output_buf_size);
+		memcpy(new_buf + client->output_buf_size, server->input_buf, server->input_buf_size);
+		free(client->output_buf);
+		client->output_buf = new_buf;
+		client->output_buf_size = target_size;
+		client->output_buf_capacity = target_size;
+		// signal to all sockets that they should write
+		server->epoll_trigger.events = (EPOLLOUT | EPOLLIN | EPOLLET);
+		server->epoll_trigger.data.fd = client_socket;
+		int rc = epoll_ctl(server->epoll_fd, EPOLL_CTL_MOD, client_socket, &server->epoll_trigger);
+		if(rc < 0)
+		{
+			return CHAT_ERR_SYS;
+		}
+	}
+	free(server->input_buf);
+	server->input_buf_capacity = 0;
+	server->input_buf_size = 0;
+	return 0;
+}
+
+/*
+* 1) Read message from client with as many retries as it takes
+* 2) store message somewhere for pop_message to access it
+*/
+int read_message_from_client(struct chat_server* server, struct epoll_event* event)
+{
+	int client_fd = event->data.fd;
+	int rc = 0;
+	int total_received = 0;
+	server->input_buf = calloc(BUF_SIZE, sizeof(char));
+	server->input_buf_capacity = BUF_SIZE;
+	do{
+		server->input_buf_size += rc;
+		total_received += rc;
+		if(server->input_buf_size == server->input_buf_capacity)
+		{
+			server->input_buf_capacity *= 2;
+			char* new_buf = calloc(server->input_buf_capacity, sizeof(char));
+			memcpy(new_buf, server->input_buf, server->input_buf_size);
+			free(server->input_buf);
+			server->input_buf = new_buf;
+		}
+		rc = recv(client_fd, server->input_buf + server->input_buf_size,
+						server->input_buf_capacity - server->input_buf_size, MSG_DONTWAIT);
+	}while(rc > 0);
+
+	if(total_received)
+	{
+		int start = 0;
+		for(int i = 0; i < server->input_buf_size; i++)
+		{
+			if(server->input_buf[i] == '\n')
+			{
+				// printf("received one message of size %d\n", i - start + 1);
+				struct chat_message* new_message = malloc(sizeof(struct chat_message));
+				char* message_content = calloc(i - start + 1, sizeof(char));
+				message_content = memcpy(message_content, server->input_buf+start, (i - start +1));
+				// add terminating character instead of '\n'
+				message_content[i-start] = '\0';
+				new_message->data = message_content;
+				server->received_messages = realloc(
+								server->received_messages, (server->n_received_messages + 1) * sizeof(struct chat_message*));
+				server->received_messages[server->n_received_messages] = new_message;
+				server->n_received_messages += 1;
+				start = i + 1;
+			}
+		}
+	}
+	return total_received;
+}
+
+/*
+ * 1) Wait on epoll/kqueue/poll for update on any socket.
+ * 2) Handle the update.
+ */
 int
 chat_server_update(struct chat_server *server, double timeout)
 {
-	/*
-	 * 1) Wait on epoll/kqueue/poll for update on any socket.
-	 * 2) Handle the update.
-	 * 2.1) If the update was on listen-socket, then you probably need to
-	 *     call accept() on it - a new client wants to join.
-	 * 2.2) If the update was on a client-socket, then you might want to
-	 *     read/write on it.
-	 */
-	(void)server;
-	(void)timeout;
-	return CHAT_ERR_NOT_IMPLEMENTED;
+	if(server->socket == -1)
+	{
+		return CHAT_ERR_NOT_STARTED;
+	}
+	if(timeout < 0)
+	{
+		timeout = 0;
+	}
+	// printf("tryna update server\n");
+	int max_events = server->n_peers + 1;
+	struct epoll_event *events;
+	events = calloc(max_events, sizeof(struct epoll_event));
+	int rc = epoll_wait(server->epoll_fd, events, max_events, (int)timeout * 1000);
+	if(rc == 0)
+	{
+		free(events);
+		// printf("timeout mbe or just no events?\n");
+		return CHAT_ERR_TIMEOUT;
+	}
+	else if(rc < 0)
+	{
+		perror("Error getting events at server update");
+		free(events);
+		return CHAT_ERR_SYS;
+	}
+	// printf("And I allegedly received %d events\n", rc);
+	for(int i = 0; i < max_events; i++)
+	{
+		if(events[i].data.fd == server->socket)
+		{
+			accept_new_peer(server);
+			continue;
+		}
+		if((events[i].events & EPOLLIN))
+		{
+			if(read_message_from_client(server, &events[i]) == 0)
+			{
+				rc = epoll_ctl(server->epoll_fd, EPOLL_CTL_DEL, events[i].data.fd, NULL);
+				if(rc < 0)
+				{
+					return CHAT_ERR_SYS;
+				}
+				close(events[i].data.fd);
+				delete_peer(server, events->data.fd);
+			}
+			send_message_to_clients(server, &events[i]);
+		}
+		if((events[i].events & EPOLLOUT))
+		{
+			send_message_to_client(server, events[i].data.fd);
+		}
+	}
+	free(events);
+	return 0;
 }
 
 int
@@ -122,11 +476,18 @@ chat_server_get_socket(const struct chat_server *server)
 int
 chat_server_get_events(const struct chat_server *server)
 {
-	/*
-	 * IMPLEMENT THIS FUNCTION - add OUTPUT event if has non-empty output
-	 * buffer in any of the client-sockets.
-	 */
 	(void)server;
+	if(server->socket == -1)
+	{
+		return 0;
+	}
+	for(int i = 0; i < server->n_peers; i++)
+	{
+		if(server->peers[i]->output_buf_size)
+		{
+			return (CHAT_EVENT_INPUT | CHAT_EVENT_OUTPUT);
+		}
+	}
 	return CHAT_EVENT_INPUT;
 }
 
